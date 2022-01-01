cbe49ec67910493a77f64e05acab12a8ae5962c3
Shamaali h <shamahazarathali@gmail.com>
2022-01-01 23:21:05 +0800

    Assignment

---
+++ str_to_list.py
@@ -0,0 +1,28 @@
+s='print happy against monkey need different effect sheep paper horse parallel journey kind account opinion lock knowledge look weather join market space married who nerve responsible payment again while rhythm prison opposite special why authority rest school operation river year shake than shame push when question road waiting Zoo respect that ring then very theory water tomorrow wheel umbrella the view walk twist unit voice waste together week weight yesterday tooth you young Zibra will yellow brain verse Xray cloud adjustment between where connection branch care needle about with addition cart button control desire Xman cloth balance'
+
+my_str= input ('Enter the string ')
+
+if my_str != s:
+    print('The entered string is an error')
+else:
+    # lowered the case
+    s_alpha = s.lower()
+    print(s.lower())
+
+    words = s_alpha.split()
+
+
+
+    words.sort()
+
+    print('The sorted words')
+
+    for word in words:
+        print(word)
+
+
+
+
+
+
+
